199e13e81a3e41aca6ac0c5026ab132b9b98b970
Irreq <irreq@protonmail.com>
2022-07-12 15:50:44 +0200

    r_a kernel init with random vector setup

--- src/beam.cu
+++ src/beam.cu
@@ -1,9 +1,85 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include "cuda_runtime.h"
+#include <math.h>
 #include <stdio.h>
 
+// Thread block size
+#define BLOCK_SIZE 256
+
 extern "C" void cudaBeamWrapper(int *res, const int *first, const int *last, int n_bytes);
+
+// Allocates an array with random integer entries.
+void randomInit(int *data, int size)
+{
+    for (int i = 0; i < size; ++i)
+        data[i] = rand();
+}
+
+__global__ void deviceDiffKernel(int *in_1, int *in_2, int *out, int N)
+{
+
+    int idx = blockIdx.x * blockDim.x + threadIdx.x + 1;
+    int idy = blockIdx.y * blockDim.y + threadIdx.y + 1;
+
+    out[idy * N + idx] = fabs((double)(in_1[idy * N + idx] - in_2[idy * N + idx]));
+}
+
+__global__ void r_vecKernel(double *phi, double *theta, int *out, int n_bytes, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N)
+    {
+        float *cosVal;
+        float *sinsinVal;
+        float *sincosVal;
+        out[i] = {sincosVal, sinsinVal, cosVal};
+        sinsinVal = sinf((double)(&theta)) * sinf((double)(&phi));
+    }
+}
+
+// Manage Thread Divergence
+__global__ void reductionKernel(int *input, int *results, int n) 
+{
+    extern __shared__ int sdata[];
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int tx = threadIdx.x;
+
+    // load input into __shared__ memory
+    int x = INT_MIN;
+    if (i < n)
+        x = input[i];
+    sdata[tx] = x;
+    __syncthreads();
+
+    // block-wide reduction
+    for (unsigned int offset = blockDim.x >> 1; offset > 0; offset >>= 1)
+    {
+        __syncthreads();
+        if (tx < offset)
+        {
+            if (sdata[tx + offset] > sdata[tx])
+                sdata[tx] = sdata[tx + offset];
+        }
+    }
+
+    // finally, thread 0 writes the result
+    if (threadIdx.x == 0)
+    {
+        // the result is per-block
+        results[blockIdx.x] = sdata[0];
+    }
+}
+
+    int r_vec(double phi, double theta, int n_bytes)
+{
+    int r[3];
+    double cosVal;
+    double sinVal;
+    double sincosVal;
+    // sincos(twopit * f, &sinVal, &cosVal);
+
+    return 0
+}
 
 __global__ void beamKernel(int *res, const int *a, const int *b, int size)
 {
