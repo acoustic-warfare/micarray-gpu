a28120553419a07763d3fc9680dab5473783f00f
Irreq <irreq@protonmail.com>
2022-07-13 10:57:35 +0200

    gpu changes

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
 import config
+config.path = os.path.dirname(os.path.realpath(__file__))
 
 from src.antenna import Antenna
 
 if __name__ == "__main__":
     at = Antenna(args=config)
     at.loop()
+
