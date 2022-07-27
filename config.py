8bef5fb10d4bcfa264095a7e72e1ebe5ba131c11
mikasoderstrom <107407666+mikasoderstrom@users.noreply.github.com>
2022-07-27 11:17:26 +0200

    improved listening algorithm & updated config file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# Global Configuration
+# --- EMULATING DATA variables ---
 f_sampling = 1000           # sampling frequency in Hz
 t_start = 0                 # start time of simulation 
 t_end = 1                   # end time of simulation
@@ -23,26 +23,37 @@ phi_deg2 = 40              # phi angel of source placement, relative to origin o
 t_start2 = 0                # start time of emission
 t_end2 = 1.5                # end time of emission
 
-# Antenna array setup variables
+# --- ANTENNA ARRAY setup variables ---
 r_a1 = [-0.08, 0, 0]        # coordinate position of origin of array1
 r_a2 = [0.08, 0, 0]         # coordinate position of origin of array2
 r_a3 = [-0.24, 0, 0]        # coordinate position of origin of array3
 r_a4 = [0.24, 0, 0]         # coordinate position of origin of array4
 rows = 8                    # number of rows
 columns = 8                 # number of columns
+elements = rows*columns     # number of elements
 distance = 20 * 10**(-3)    # distance between elements (m)
 
-# Filter variables
+# --- FILTER variables ---
 filter_order = 200          # filter order
 scale_factor = 10000        # scale factor, adjusting filter width
 f_bands_N = 45                                  # number of frequency bands
 bandwidth = [100, f_sampling/2-f_sampling/100]  # bandwidth of incoming audio signal
 
+# --- OTHER variables ---
+# Modes for adaptive weights
+modes = 7
+
 # Beamforming resolution
-x_res = 10                          # resolution in x
-y_res = 10                          # resolution in y
+x_res = 5                          # resolution in x
+y_res = 5                          # resolution in y
 
+# Listening values
+theta_listen = 0
+phi_listen = 45
 
+# Initial values (startup values of microphone signals) to ignore
+audio_signals = 'recorded'
+initial_values = 40000
 
 verbose = True
 
